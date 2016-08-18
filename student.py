cdc74a1e244ca646a6b92a236ef4a7dd99c369a1
manuellah <emmanumuthui@gmail.com>
2016-08-18 12:19:18 +0300

    created two instances

--- student.py
+++ student.py
@@ -1,7 +1,17 @@
-class student(object):#inheritance
+class Student(object):#inheritance
     def __init__(self,id,fName,lName,county="KE"):
     	self.__id=id
     	self.fname=fName
     	self.lName=lName
 
     def attend_class(self, **kwargs):
+    	'''
+    	default values:
+    	 loc = 'Hogwarts'
+    	 date= current_date
+    	 teacher='alex'
+    	'''
+    	pass
+
+s1 = Student(1,'manuh','muthui')
+s1 = Student(1,'allan','m','ug')
